ab4c9ba41e82ad3acb4e67d0f923b81e64951ff5
dema28 <dema28ster@gmail.com>
2024-09-02 18:24:00 +0200

    lessens_1 Stepik

---
+++ src/Stepik/lessens_1.java
@@ -0,0 +1,15 @@
+package Stepik;
+
+import java.util.Scanner;
+
+public class lessens_1 {
+    public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Как тебя завут?");
+                String name =scanner.next();
+                System.out.println("Здравствуй, " + name);
+
+
+    }
+}
